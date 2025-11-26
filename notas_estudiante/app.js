1333c1a76fdfa117e5714331146ae751b9a52c9b
Lucia <lulizcano.aa@hotmail.com>
2025-11-25 21:42:48 -0500

    se realizo la documentacion

--- notas_estudiante/app.js
+++ notas_estudiante/app.js
@@ -1,7 +1,11 @@
-let nota1 = 10;
-let nota2 = 50;
-let nota3 = 20;
+// Definimos las tres notas
+let nota1 = 10;   // Primera nota
+let nota2 = 50;   // Segunda nota
+let nota3 = 20;   // Tercera nota
 
+// Calculamos el promedio
+// Fórmula: (nota1 + nota2 + nota3) / cantidad de notas
 let promedio = (nota1 + nota2 + nota3) / 3;
 
+// Mostramos el resultado en consola
 console.log("El promedio de las tres notas es: " + promedio);
